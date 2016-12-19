13456054260a03c9f1f2f32e6d3cb3e50a9d7b63
miaochangchun <miaochangchun@hotmail.com>
2016-12-19 11:55:45 +0800

    A project, unity's tutorial Rolling Ball

---
+++ Lessons/Assets/Scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
+
+public class playerController : MonoBehaviour {
+
+	private Rigidbody ball;
+
+	void Start () 
+	{
+		ball = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizantal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizantal, 0.0f, moveVertical);
+
+		ball.AddForce (movement);
+	}
+}
